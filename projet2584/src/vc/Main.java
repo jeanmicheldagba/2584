4ae8c7e535d27af03ac2215d7ddf6ffffe447b6c
Hugo <Hugo@Hugo-PC>
2018-02-19 17:49:49 +0100

    Reprend code d'Arche 2048

--- projet2584/src/vc/Main.java
+++ projet2584/src/vc/Main.java
@@ -6,6 +6,7 @@
 package vc;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -22,6 +23,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+        boolean add = scene.getStylesheets().add("css/styles.css");
         
         stage.setScene(scene);
         stage.show();
